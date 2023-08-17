4d2240155e9dc19b9dfcc38eb625362d0801414f
Rithvik-padma <rithvik.padma@gmail.com>
2023-08-17 22:11:13 +0530

    Add the functionality to withdraw the ETH sent to the contract

--- fund.sol
+++ fund.sol
@@ -8,12 +8,28 @@ contract Fund{
     using PriceConverter for uint256;
 
     uint256 public minmUSD = 5;
+    address[] public funders; 
     mapping(address => uint256 amountFunded) public addressToAmountFunded;    
+    address public owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
 
     function fund() public payable{
         require(msg.value.getConversionRate() >= minmUSD, "Enough ETH was not sent!");
+        funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    // function withdraw() public{}
+    function withdraw() public{
+        require(owner == msg.sender, "Must be owner!");
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        (bool withdrawSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(withdrawSuccess, "Withdraw failed!");
+    }
 }
